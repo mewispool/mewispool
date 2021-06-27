0e437e8620f5b1dae1d44d6efe229aa25821bb36
mewispool <mewispool@gmail.com>
2021-06-27 23:05:37 +0430

    - bash script updated

--- run_graph_classification.sh
+++ run_graph_classification.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-python3 -m venv mewispool_env
+if [ ! -d "mewispool_env" ]; then
+  python3 -m venv mewispool_env
+  source mewispool_env/bin/activate
+  pip3 install torch
+  pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.9.0+cu102.html
+  pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.9.0+cu102.html
+  pip install torch-geometric
+fi
 source mewispool_env/bin/activate
-
-pip3 install torch
-pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.9.0+cu102.html
-pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.9.0+cu102.html
-pip install torch-geometric
 
 python3 graph_classification/train.py
