dd0e6f99847b3888655b81b588c2386a09bdb254
mewispool <mewispool@gmail.com>
2021-06-27 21:43:27 +0430

    - train refactored.

--- graph_classification/train.py
+++ graph_classification/train.py
@@ -3,93 +3,136 @@ from math import ceil
 import json
 import time
 import torch
+import os
 from torch_geometric.datasets import TUDataset
 from torch_geometric.data import DataLoader
-from graph_classification.models import GNN, Net
-from torch_geometric.nn import GCNConv, DenseGraphConv, dense_mincut_pool
-from torch_geometric.utils import to_dense_batch, to_dense_adj, get_laplacian
+from graph_classification.models_new import Net, Net2, Net3
+from torch_geometric.transforms import OneHotDegree
 
-path = osp.join(osp.dirname(osp.realpath(__file__)), 'data', 'PROTEINS')
-dataset = TUDataset(path, name='PROTEINS').shuffle()
-max_num_nodes = max(dataset.data.num_nodes)
-average_nodes = int(dataset.data.x.size(0) / len(dataset))
+torch.manual_seed(7)
+OTHER_DATASET_NAMES = ['NCI1', 'NCI109', 'PROTEINS_full', 'REDDIT-BINARY']
+#'DD',
+# 'PROTEINS', 'ENZYMES', 'COLLAB', 'IMDB-BINARY', 'IMDB-MULTI',
+#                  'FRANKENSTEIN', 'MUTAG', 'Mutagenicity'
+DATASET_NAME = 'MUTAG'
+
+BATCH_SIZE = 20
+EPOCHS = 200
+FOLDS = 10
+LEARNING_RATE = 1e-2
+WEIGHT_DECAY = 1e-5
+SCHEDULER_PATIENCE = 10
+SCHEDULER_FACTOR = 0.1
+EARLY_STOPPING_PATIENCE = 50
+
+path = osp.join(osp.dirname(osp.realpath(__file__)), 'data', DATASET_NAME)
+dataset = TUDataset(path, name=DATASET_NAME, use_node_attr=True, use_edge_attr=True).shuffle()
+
 n = (len(dataset) + 9) // 10
+
+input_dim = dataset.num_features
+num_classes = dataset.num_classes
+
+dataset = dataset.shuffle()
+
 test_dataset = dataset[:n]
 val_dataset = dataset[n:2 * n]
 train_dataset = dataset[2 * n:]
-test_loader = DataLoader(test_dataset, batch_size=20)
-val_loader = DataLoader(val_dataset, batch_size=20)
-train_loader = DataLoader(train_dataset, batch_size=20)
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True)
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
 device = torch.device('cpu')
 
-with open('../configs/config_files/arch1-conf1.json', 'rb') as handle:
-    net_config = json.load(handle)
-
-model = Net(net_config, input_dim=dataset.num_features, num_classes=dataset.num_classes).to(device)
-
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=0)
-
+# Model, Optimizer and Loss definitions
+model = Net3(input_dim=input_dim, hidden_dim=32, num_classes=num_classes).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer,
+                                                       patience=SCHEDULER_PATIENCE,
+                                                       factor=SCHEDULER_FACTOR,
+                                                       verbose=True)
 nll_loss = torch.nn.NLLLoss()
 
-
-def train(epoch):
+best_val_loss = float('inf')
+best_test_acc = 0
+wait = None
+for epoch in range(EPOCHS):
+    # Training the model
+    s_time = time.time()
+    train_loss = 0.
+    train_corrects = 0
     model.train()
-    loss_all = 0
-
     for i, data in enumerate(train_loader):
-        # print(f'{i} / {len(train_loader)}')
+        s = time.time()
         data = data.to(device)
         optimizer.zero_grad()
-
         out, loss_pool = model(data.x, data.edge_index, data.batch)
-
         loss_classification = nll_loss(out, data.y.view(-1))
-
         loss = loss_classification + 0.01 * loss_pool
 
         loss.backward()
-        loss_all += data.y.size(0) * loss.item()
+        train_loss += loss.item()
+        train_corrects += out.max(dim=1)[1].eq(data.y.view(-1)).sum().item()
         optimizer.step()
-    return loss_all / len(train_dataset)
+        # print(f'{i}/{len(train_loader)}, {time.time() - s}')
 
+    train_loss /= len(train_loader)
+    train_acc = train_corrects / len(train_dataset)
+    scheduler.step(train_loss)
 
-@torch.no_grad()
-def test(loader):
+    # Validation
+    val_loss = 0.
+    val_corrects = 0
     model.eval()
-    correct = 0
+    with torch.no_grad():
+        for i, data in enumerate(val_loader):
+            s = time.time()
+            data = data.to(device)
+            out, loss_pool = model(data.x, data.edge_index, data.batch)
+            loss_classification = nll_loss(out, data.y.view(-1))
+            loss = loss_classification + 0.01 * loss_pool
+            val_loss += loss.item()
+            val_corrects += out.max(dim=1)[1].eq(data.y.view(-1)).sum().item()
+            # print(f'{i}/{len(val_loader)}, {time.time() - s}')
 
-    for data in loader:
-        data = data.to(device)
-        pred, loss_pool = model(data.x, data.edge_index, data.batch)
-        loss_classification = nll_loss(pred, data.y.view(-1))
-        loss = loss_classification + 0.01 * loss_pool
-        correct += pred.max(dim=1)[1].eq(data.y.view(-1)).sum().item()
+    val_loss /= len(val_loader)
+    val_acc = val_corrects / len(val_dataset)
 
-    return loss, correct / len(loader.dataset)
+    # Test
+    test_loss = 0.
+    test_corrects = 0
+    model.eval()
+    with torch.no_grad():
+        for i, data in enumerate(test_loader):
+            s = time.time()
+            data = data.to(device)
+            out, loss_pool = model(data.x, data.edge_index, data.batch)
+            loss_classification = nll_loss(out, data.y.view(-1))
+            loss = loss_classification + 0.01 * loss_pool
+            test_loss += loss.item()
+            test_corrects += out.max(dim=1)[1].eq(data.y.view(-1)).sum().item()
+            # print(f'{i}/{len(val_loader)}, {time.time() - s}')
 
+    test_loss /= len(test_loader)
+    test_acc = test_corrects / len(test_dataset)
 
-best_val_acc = test_acc = 0
-best_val_loss = float('inf')
-patience = start_patience = 50
-for epoch in range(1, 15000):
-    s = time.time()
-    train_loss = train(epoch)
-    _, train_acc = test(train_loader)
-    val_loss, val_acc = test(val_loader)
+    elapse_time = time.time() - s_time
+    log = '[*] Epoch: {}, Train Loss: {:.3f}, Train Acc: {:.2f}, Val Loss: {:.3f}, ' \
+          'Val Acc: {:.2f}, Test Loss: {:.3f}, Test Acc: {:.2f}, Elapsed Time: {:.1f}'\
+        .format(epoch, train_loss, train_acc, val_loss, val_acc, test_loss, best_test_acc, elapse_time)
+    print(log)
+
+    # Early-Stopping
     if val_loss < best_val_loss:
-        test_loss, test_acc = test(test_loader)
-        best_val_acc = val_acc
-        patience = start_patience
+        best_val_loss = val_loss
+        best_test_acc = test_acc
+        wait = 0
+        # saving the model with best validation loss
+        torch.save(model.state_dict(), f'checkpoints/{DATASET_NAME}.pkl')
     else:
-        patience -= 1
-        if patience == 0:
-            break
-    print('Epoch: {:03d}, '
-          'Train Loss: {:.3f}, Train Acc: {:.3f}, '
-          'Val Loss: {:.3f}, Val Acc: {:.3f}, '
-          'Test Loss: {:.3f}, Test Acc: {:.3f}, time: {:.1f}'.format(epoch, train_loss,
-                                                                     train_acc, val_loss,
-                                                                     val_acc, test_loss,
-                                                                     test_acc,
-                                                                     time.time() - s))
+        wait += 1
+    # early stopping
+    if wait == EARLY_STOPPING_PATIENCE:
+        print('======== Early stopping! ========')
+        break
+
